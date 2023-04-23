3eb036d61209c9b1ae1aea48f44bb9c3c1c3e39b
Nikitosic2003 <96696040+Nikitosic2003@users.noreply.github.com>
2023-04-23 22:52:35 +0300

    Add files via upload

---
+++ CosmosCalendar/src/main/java/ru/mai/javafx/javafxcalendarapplication/TodoController.java
@@ -0,0 +1,20 @@
+package ru.mai.javafx.javafxcalendarapplication;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class TodoController {
+
+    @FXML
+    private Button justABtn;
+
+    @FXML
+    private Label justLabel;
+
+    @FXML
+    public void init() {
+
+    }
+
+}
