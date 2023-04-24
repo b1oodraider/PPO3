e9dd8ac9b440d798c617e58d6707353e1471d87b
NikitaOrlovsky <96696040+Nikitosic2003@users.noreply.github.com>
2023-04-24 23:17:24 +0300

    Delete CosmosCalendar directory

--- CosmosCalendar/src/main/java/ru/mai/javafx/javafxcalendarapplication/modules/Configs.java
+++
@@ -1,8 +0,0 @@
-package ru.mai.javafx.javafxcalendarapplication.modules;
-public class Configs {
-    //protected String dbHost = "localhost";
-    //protected String dbPort = "3306";
-    protected String dbUser = "root";
-    protected String dbPass = "555555Five";
-    //protected String dbName = "Calendar";
-}
