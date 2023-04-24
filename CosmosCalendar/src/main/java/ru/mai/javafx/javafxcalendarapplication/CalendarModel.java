02b0b3468a55e13796b8871c9a4b749d1075d6c6
NikitaOrlovsky <96696040+Nikitosic2003@users.noreply.github.com>
2023-04-24 23:17:00 +0300

    Delete CosmosCalendar directory

--- CosmosCalendar/src/main/java/ru/mai/javafx/javafxcalendarapplication/CalendarModel.java
+++
@@ -1,4 +0,0 @@
-package ru.mai.javafx.javafxcalendarapplication;
-
-public class CalendarModel {
-}
--- CosmosCalendar/src/main/java/ru/mai/javafx/javafxcalendarapplication/CalendarView.java
+++
@@ -1,9 +0,0 @@
-package ru.mai.javafx.javafxcalendarapplication;
-
-import ru.mai.javafx.javafxcalendarapplication.modules.MonthlyCalendar;
-
-public class CalendarView extends CalendarController {
-    /*public void initCalendar() {
-        super.getRoot().add(new MonthlyCalendar(), 0, 1);
-    }*/
-}
